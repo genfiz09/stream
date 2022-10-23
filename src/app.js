986456da5f04f33e617ff08cfebf802a7f2d1542
genfiz09 <genfiz09@gmail.com>
2022-10-22 22:12:14 -0400

    nuevo cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const http = require('./main');
-const express = require('express');
-const app = express();
 
-app.set('port', process.env.PORT || 3000);
+//const app = express();
 
-http.listen(3000, () => {
-  
-console.log(`Server on port ${app.get('port')}`);
-});
+
+
+
+http.listen(process.env.PORT || 3000)
+console.log('puerto coriendo en el puerto', process.env.PORT || 3000 );
 
